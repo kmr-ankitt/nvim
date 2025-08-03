51e61063273d2ce94d0bca0cdb6809ddcb0ee26d
ankit <sahuankit.murhu@gmail.com>
2025-08-03 15:23:42 +0530

    feat: startup screen

--- init.lua
+++ init.lua
@@ -954,6 +954,7 @@ require('lazy').setup({
       -- - sd'   - [S]urround [D]elete [']quotes
       -- - sr)'  - [S]urround [R]eplace [)] [']
       require('mini.surround').setup()
+      require('mini.starter').setup()
 
       -- Simple and easy statusline.
       --  You could remove this setup call if you don't like it,
